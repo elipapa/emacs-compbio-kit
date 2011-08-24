484632ff69a514a2ecc376854223494bf5f5e2e0
Eli Papa <elipapa@mit.edu>
2011-08-24 18:07:35 +0100

    patched python-mode recipe until it gets fixed on el-get master branch

--- init.el
+++ init.el
@@ -97,6 +97,19 @@
                :info "doc/info/"
                :load-path ("lisp")
                :features ess-site)
+
+        ;; python-mode recipe needs to be fixed, removing doctest-mode, in
+        ;; el-get master branch
+        (:name python-mode
+               :type emacsmirror
+               :description "Major mode for editing Python programs"
+               :features (python-mode)
+               :compile nil
+               :post-init (lambda ()
+                            (add-to-list 'auto-mode-alist '("\\.py$" . python-mode))
+                            (add-to-list 'interpreter-mode-alist '("python" . python-mode))
+                            (autoload 'python-mode "python-mode" "Python editing mode." t)))
+
         ))
 
 
